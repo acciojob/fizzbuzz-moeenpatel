c6d2c3a7bc0a19dd5103a191b09a9283b91b119a
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-18 16:59:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+function fizzBuzz(){
+	let arr=[];
+
+	for(let i = 1;i<100;i++){
+
+		if(i % 3==0  && i % 5==0){
+
+			arr.push("FizzBuzz");
+		}
+
+		else if( i % 3 ==0){
+
+			arr.push("Fizz");
+		}
+		else if(i % 5==0){
+			arr.push("Buzz");
+		}
+		else{
+			arr.push("i");
+		}
+		
+	}
+	console.log(arr);
+}
+fizzBuzz();
